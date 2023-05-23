f50bebd33983414ec8a7a3279b63ad0302355136
Tem <54119870+Teampris@users.noreply.github.com>
2023-05-23 02:28:47 +0200

    Add basic functionality

--- script.js
+++ script.js
@@ -2,6 +2,30 @@ let numberOne;
 let numberTwo;
 let operator;
 
+const display = document.querySelector('.display');
+
+const buttons = document.querySelectorAll('.buttons button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const buttonValue = button.textContent;
+
+        switch(buttonValue) {
+            case '=':
+                display.textContent = eval(display.textContent);
+                break;
+            case 'C':
+                display.textContent = '';
+                break;
+            case 'DEL':
+                display.textContent = display.textContent.slice(0,-1);
+                break;
+            default:
+                display.textContent += buttonValue;
+        }
+    })
+})
+
 function add(a,b) {
     return a + b;
 }
